76777308824aebb32b7e613537269e778d9939f0
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-29 15:01:59 +0530

    Initial commit

---
+++ src/TrailingZeroes/SimpleCode.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 10/29/2020
+ *   Time: 3:01 PM
+ *   File: SimpleCode.java
+ */
+
+package TrailingZeroes;
+
+public class SimpleCode {
+}
