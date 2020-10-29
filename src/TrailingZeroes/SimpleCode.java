4310077725d0a0db40046acf5252049b0ce34522
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-10-29 15:23:00 +0530

    program to find no zeroes in a factorial of no. final commit

--- src/TrailingZeroes/SimpleCode.java
+++ src/TrailingZeroes/SimpleCode.java
@@ -7,5 +7,25 @@
 
 package TrailingZeroes;
 
+import java.util.Scanner;
+
 public class SimpleCode {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int factNo=sc.nextInt();
+        System.out.println(countZeros(factNo));
+    }
+    public static int countZeros(int factNo){
+        int fact=1;
+        for(int i=2;i<=factNo;i++){
+            fact=fact*i;
+    }
+        int res=0;
+        while(fact%10==0){
+            res++;
+            fact=fact/10;
+
+        }
+        return res;
+}
 }
