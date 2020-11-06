097af1e09394d64e723a0c80af6eec49b2cf7534
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-07 00:12:05 +0530

    make a simple class for findiing hcf and a square tile program that can fill the whole area

---
+++ src/HCF/Simple.java
@@ -0,0 +1,15 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/7/2020
+ *   Time: 12:01 AM
+ *   File: Simple.java
+ */
+
+package HCF;
+
+public class Simple {
+    public int hcfNo(int a,int b){
+        int lowerNo=Math.min(a,b);
+
+    }
+}
