30082385ce7eff48bebd24871c0b66e96cd25720
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-07 11:45:22 +0530

    efficient class for less timing add method hcf no

---
+++ src/HCF/Efficient.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/7/2020
+ *   Time: 11:37 AM
+ *   File: Efficient.java
+ */
+
+package HCF;
+
+public class Efficient {
+    public static void hcfNo(int a,int b) {
+
+    }
+}
