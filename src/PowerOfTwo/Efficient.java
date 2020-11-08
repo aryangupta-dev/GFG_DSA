9c3ec90f33569331ba87ce0e36b43b14eb1e1e97
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-08 19:18:02 +0530

    efficient code for power of 2

---
+++ src/PowerOfTwo/Efficient.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/8/2020
+ *   Time: 7:14 PM
+ *   File: Efficient.java
+ */
+
+package PowerOfTwo;
+
+import java.util.Scanner;
+
+public class Efficient {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
