20026b34b2ffe3765394a8cb6161bf913a954dd3
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-08 19:05:16 +0530

    make method isPowerTwo to check the no

--- src/PowerOfTwo/Simple.java
+++ src/PowerOfTwo/Simple.java
@@ -10,10 +10,19 @@ package PowerOfTwo;
 import java.util.Scanner;
 
 public class Simple {
+    static boolean isPowerTwo(int n){
+        if(n<=0)
+            return false;
+        while(n!=1){
+            if(n%2!=0){
+                return false;
+            }n=n/2;
+        }return true;
+    }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
+        System.out.println(isPowerTwo(sc.nextInt()));
     }
 
 
