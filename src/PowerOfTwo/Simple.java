a29efec197fcece11b00304d97cca9c807fac87b
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-08 19:04:44 +0530

    make class simple to check input is power of two

---
+++ src/PowerOfTwo/Simple.java
@@ -0,0 +1,20 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/8/2020
+ *   Time: 6:34 PM
+ *   File: Simple.java
+ */
+
+package PowerOfTwo;
+
+import java.util.Scanner;
+
+public class Simple {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+
+
+}
