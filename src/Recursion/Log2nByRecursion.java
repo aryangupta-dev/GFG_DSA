09e97c2d842ddd4c3aee837e4b2a8cb8401890c9
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-11 23:29:48 +0530

    add comments efficient for the user

--- src/Recursion/Log2nByRecursion.java
+++ src/Recursion/Log2nByRecursion.java
@@ -9,14 +9,16 @@ package Recursion;
 
 public class Log2nByRecursion {
     static int find(int n) {
-        if (n == 1)
+        if (n == 1)//here if you want with base 2 make it (n<3). :/
             return 0;
         else
             return 1 + find(n / 2);
     }
 
     public static void main(String[] args) {
-
+        /*
+         * this is a program of log which have base 2
+         * if you want to find it with other bases, just change the condition of line 12 */
         System.out.println(find(32));
     }
 }
