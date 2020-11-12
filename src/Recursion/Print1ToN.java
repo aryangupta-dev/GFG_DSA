9bf98c095de330424f86132ca06ac1923f22923d
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-12 23:41:12 +0530

    program by tail recursion of 1 to n

---
+++ src/Recursion/Print1ToN.java
@@ -0,0 +1,22 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/12/2020
+ *   Time: 11:28 PM
+ *   File: Print1ToN.java
+ */
+
+package Recursion;
+
+public class Print1ToN {
+    static void gprint(int n, int k) {
+
+        if (n > 0) {
+            System.out.println(k);
+            gprint(n - 1, k + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        gprint(5, 1);
+    }
+}
