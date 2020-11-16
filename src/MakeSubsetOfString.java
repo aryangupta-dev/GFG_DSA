c6793c66438480f4a5b4e1cdf0d9113cb58f3e0e
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-17 00:00:21 +0530

    add comments

--- src/MakeSubsetOfString.java
+++ src/MakeSubsetOfString.java
@@ -7,14 +7,14 @@
 
 public class MakeSubsetOfString {
     public static void main(String[] args) {
-        String str = "abcd";
-        char[] ch = str.toCharArray();
-        int n = str.length();
+        String str = "abcde";//taing input of  the string.
+        char[] ch = str.toCharArray();// converting it to character array to fetch char later.
+        int n = str.length();//length of the string
         int totalSubset = (int) Math.pow(n, 2);
         for (int index = 0; index < totalSubset; index++) {
             System.out.print("{");
             for (int j = 0; j < n; j++) {
-                if ((index & (1 << j)) > 0) {
+                if ((index & (1 << j)) > 0) {//assuming every string letter in the form og binary.
                     System.out.print(ch[j]);
                 }
             }
