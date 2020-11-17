272f77535d77cc25359914329141aac581253434
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-17 23:59:54 +0530

    program for sum of digits

--- src/Recursion/SumOfDigits.java
+++ src/Recursion/SumOfDigits.java
@@ -13,4 +13,8 @@ public class SumOfDigits {
             return n;
         return n % 10 + sum(n / 10);
     }
+
+    public static void main(String[] args) {
+        System.out.println(sum(253));
+    }
 }
