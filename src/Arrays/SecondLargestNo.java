bee9a203370b044a930883721492428d4c3ab260
Aryan gupta-dev <guptaaryan2468@gmail.com>
2020-11-27 21:20:48 +0530

    add greatestno program from the array,

---
+++ src/Arrays/SecondLargestNo.java
@@ -0,0 +1,24 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan gupta (ag2602)
+ *   Date: 11/27/2020
+ *   Time: 9:16 PM
+ *   File: SecondLargestNo.java
+ */
+
+package Arrays;
+
+public class SecondLargestNo {
+    static int secondLargest(int arr[]) {
+
+    }
+
+    static int greatest(int[] arr) {
+        int largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (largest < arr[i]) {
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+}
