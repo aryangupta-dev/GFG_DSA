374c3abfc53143289a9794fd4de058ece167a357
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-01 21:17:30 +0530

    Final commit to the left most non-repeat element.

--- src/String/LeftMostNonRepeat.java
+++ src/String/LeftMostNonRepeat.java
@@ -7,6 +7,31 @@
 
 package String;
 
-public class LeftMostNonRepeat {
+import java.util.Arrays;
 
+public class LeftMostNonRepeat {
+    public static void main(String[] args) {
+        int[] arr = new int[256];
+        Arrays.fill(arr, -1);
+        String str = "abab";
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < str.length(); i++) {
+            int fre = arr[str.charAt(i)];
+            if (fre == -1) {
+                arr[str.charAt(i)] = i;
+            } else {
+                arr[str.charAt(i)] = -2;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                res = Math.min(res, arr[i]);
+            }
+        }
+        if (res == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(res);
+        }
+    }
 }
