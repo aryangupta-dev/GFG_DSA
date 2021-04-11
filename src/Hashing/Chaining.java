520c16061d152470a0d1d212d2b67a368c83b061
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-11 11:15:25 +0530

    Added two methods in the class insert and remove.

--- src/Hashing/Chaining.java
+++ src/Hashing/Chaining.java
@@ -7,5 +7,22 @@
 
 package Hashing;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Chaining {
+    int bucket = 9;
+    ArrayList<LinkedList<Integer>> stack = new ArrayList<>();
+
+    void insert(int key) {
+        int res = key % bucket;
+        stack.get(res).add(key);
+        System.out.println("Inserted at index " + res);
+    }
+
+    void remove(int key) {
+        int res = key % bucket;
+        stack.get(res).remove((Integer) key);
+        System.out.println("Deleted the key " + key);
+    }
 }
