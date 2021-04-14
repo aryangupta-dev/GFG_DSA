b0a75331bc383346d3401b6a69cce8796d844e13
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-15 00:33:49 +0530

    added method quicksort for sorting.

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -8,5 +8,25 @@
 package Sorting;
 
 public class QuickSort {
+    int quickSort(int[] arr, int low, int high) {
+        int pivot = arr[arr.length - 1];
+        int i = low - 1;
+        int last = 0;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int v = arr[i];
+                arr[i] = arr[j];
+                arr[j] = v;
+            }
+        }
+        last = arr[i + 1];
+        arr[i + 1] = pivot;
+        arr[arr.length - 1] = last;
+        return i + 1;
+    }
 
+    void partition(int[] arr) {
+
+    }
 }
