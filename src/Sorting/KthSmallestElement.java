f76fd5bb7a6833f6533c98dc6ee10a53d1fc9ba9
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-17 23:18:53 +0530

    final commit to the class KthSmallestElement.java

--- src/Sorting/KthSmallestElement.java
+++ src/Sorting/KthSmallestElement.java
@@ -7,23 +7,38 @@
 
 package Sorting;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //Considering element of the arrays are distinct to each others.
 public class KthSmallestElement {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array.");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter the element of the array.");
+            int no = sc.nextInt();
+            arr[i] = no;
+        }
+        KthSmallestElement object = new KthSmallestElement();
+        System.out.println("Enter kth digit to find the smallest at that position.");
+        int k = sc.nextInt();
+        int result = object.kthSmallestNo(arr, size, k);
+        System.out.println(arr[result]);
     }
 
-    public int kthSmallestNo(ArrayList<Integer> arr, int size, int k) {
+    public int kthSmallestNo(int[] arr, int size, int k) {
         int high = size - 1;
         int low = 0;
         while (low <= high) {
-            int pivot = partitionOfTheArray(arr, 0, high);
+            int pivot = partitionOfTheArray(arr, low, high);
             if (pivot == k - 1) {
                 return pivot;
-            } else if (pivot > k) {
-                low = pivot - 1;
+            } else if (pivot > k - 1) {
+                high = pivot - 1;
             } else {
                 low = pivot + 1;
             }
@@ -31,21 +46,22 @@ public class KthSmallestElement {
         return -1;
     }
 
-    public int partitionOfTheArray(ArrayList<Integer> arr, int low, int high) {
-        int pivot = arr.get(high);
-        int last = 0;
+    public int partitionOfTheArray(int[] arr, int low, int high) {
+        int pivot = arr[high];
+
         int i = low - 1;
-        for (int j = 0; j < arr.size(); j++) {
-            if (pivot > arr.get(j)) {
+        for (int j = low; j < arr.length; j++) {
+            if (pivot > arr[j]) {
                 i++;
-                int temp = arr.get(i);
-                arr.set(i, arr.get(j));
-                arr.set(j, temp);
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            last = arr.get(high);
-            arr.set(high, arr.get(i + 1));
-            arr.set(i + 1, last);
         }
+        int last = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = last;
+
         return i + 1;
     }
 }
