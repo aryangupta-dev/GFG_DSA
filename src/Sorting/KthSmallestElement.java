b592f64880a00fa9b1276a6ec02142244b08dd41
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-17 14:59:58 +0530

    Class and comment added.

---
+++ src/Sorting/KthSmallestElement.java
@@ -0,0 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan Gupta (ag2602)
+ *   Date: 4/17/2021
+ *   Time: 2:58 PM
+ *   File: KthSmallestElement.java
+ */
+
+package Sorting;
+
+//Considering element of the arrays are distinct to each others.
+public class KthSmallestElement {
+
+}
