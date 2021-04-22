e97e99a9bfe362f1e94263a04299841cdd925eb5
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-22 21:36:35 +0530

    max sum of subarray of length k

---
+++ src/Dequeue/MaximumSubarraySum.java
@@ -0,0 +1,41 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan Gupta (ag2602)
+ *   Date: 4/22/2021
+ *   Time: 8:59 PM
+ *   File: MaximumSubarraySum.java
+ */
+
+package Dequeue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MaximumSubarraySum {
+    static void maxSum(int[] arr, int n, int k) {
+        Deque<Integer> obj = new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            while ((!obj.isEmpty()) && (arr[i] >= arr[obj.peekLast()])) {
+                obj.removeLast();
+            }
+            obj.add(i);
+        }
+        for (int i = k; i < n; i++) {
+            System.out.println(arr[obj.peek()] + " ");
+
+            while ((!obj.isEmpty()) && obj.peek() <= i - k)
+                obj.removeFirst();
+
+
+            while ((!obj.isEmpty()) && arr[i] >= arr[obj.peekLast()])
+                obj.removeLast();
+
+
+            obj.addLast(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 3, 7, 9, 4};
+        maxSum(arr, arr.length, 3);
+    }
+}
