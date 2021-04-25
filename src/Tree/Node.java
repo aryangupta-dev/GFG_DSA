df76816d2da2f1030f7bbaf4d4d7ea8e929aec6e
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-25 21:21:14 +0530

    Node class added

---
+++ src/Tree/Node.java
@@ -0,0 +1,18 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan Gupta (ag2602)
+ *   Date: 4/25/2021
+ *   Time: 9:20 PM
+ *   File: Node.java
+ */
+
+package Tree;
+
+public class Node {
+    int data;
+    Node right;
+    Node left;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
