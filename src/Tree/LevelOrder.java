7150630ea03fb757e78658fcdf7c249077f5cc35
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-04-28 12:01:12 +0530

    Tree height class added to the tree package

---
+++ src/Tree/LevelOrder.java
@@ -0,0 +1,38 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Aryan Gupta (ag2602)
+ *   Date: 4/28/2021
+ *   Time: 11:05 AM
+ *   File: LevelOrder.java
+ */
+
+package Tree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LevelOrder {
+    public static void traversal(Node root) {
+        Deque<Node> object = new LinkedList<>();
+        object.add(root);
+        while (!object.isEmpty()) {
+            Node current = object.poll();
+            System.out.println(current.data);
+            if (current.left != null)
+                object.add(current.left);
+            if (current.right != null) {
+                object.add(current.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Node obj = new Node(10);
+        obj.left = new Node(20);
+        obj.left.left = new Node(30);
+        obj.right = new Node(40);
+        obj.left.right = new Node(50);
+        obj.right.left = new Node(60);
+        traversal(obj);
+    }
+}
