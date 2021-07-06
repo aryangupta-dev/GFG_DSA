e42dafe689f13d3cad94f07d32f724247654ca5a
Aryan gupta-dev <guptaaryan2468@gmail.com>
2021-07-06 15:16:01 +0530

    Bfs traversal added

--- src/Graph/BFSTraversal.java
+++ src/Graph/BFSTraversal.java
@@ -1,18 +1,38 @@
 /*   Created by IntelliJ IDEA.
  *   Author: Aryan Gupta (ag2602)
  *   Date: 7/6/2021
- *   Time: 1:11 PM
- *   File: VertexAdded.java
+ *   Time: 1:36 PM
+ *   File: BFSTraversal.java
  */
 
 package Graph;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 
-public class VertexAdded {
+public class BFSTraversal {
     public static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) {
         adj.get(u).add(v);
         adj.get(v).add(v);
+    }
+
+    public static void traversal(ArrayList<ArrayList<Integer>> adj, int v, int s) {
+        boolean[] visited = new boolean[v];
+        Queue<Integer> obj = new ArrayDeque<>();
+        obj.add(s);
+        visited[s] = true;
+        while (!obj.isEmpty()) {
+            int q = obj.poll();
+            System.out.println(q + " ");
+            for (int a : adj.get(q)) {
+                if (!visited[a]) {
+                    visited[a] = true;
+                    obj.add(a);
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -28,7 +48,6 @@ public class VertexAdded {
         addEdge(adj, 1, 3);
         addEdge(adj, 3, 4);
         addEdge(adj, 2, 4);
-
-
+        traversal(adj, 5, 0);
     }
 }
